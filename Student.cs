cc820d56dd09b117493755ae931d4772bb556904
Tjay33-xx <punjabifoodmart@hotmail.com>
2026-07-04 17:37:15 -0400

    Add Student class with validation attributes

---
+++ Student.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirstStudentApp
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+    }
+}
